498502f1cbb29d6ea23a0756e5fc3b7ab77d13e0
Mahek Begum <mahekbegum54@gmail.com>
2025-05-19 20:39:28 +0530

    Initial commit

---
+++ Todo.jsx
@@ -0,0 +1,25 @@
+// src/components/Todo.jsx
+import React from 'react'
+
+function Todo({ todo, onToggle, onDelete }) {
+  return (
+    <li className="todo-enter" style={{
+      background: '#ddd',
+      padding: '10px',
+      margin: '10px 0',
+      borderRadius: '6px',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      textDecoration: todo.completed ? 'line-through' : 'none',
+      opacity: todo.completed ? 0.6 : 1,
+    }}>
+      <span onClick={onToggle} style={{ cursor: 'pointer' }}>
+        {todo.text}
+      </span>
+      <button onClick={onDelete} style={{ background: '#ff4d4f', color: 'white' }}>Delete</button>
+    </li>
+  )
+}
+
+export default Todo
